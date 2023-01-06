b031fa62d56f7b93ce25f131c3ee1340ed72ed52
Ildson Leno <ildson.leno@outlook.com>
2023-01-07 00:10:35 +0044

    final

--- client/script.js
+++ client/script.js
@@ -82,7 +82,7 @@ const handleSubmit = async (e) => {
   // messageDiv.innerHTML = "..."
   loader(messageDiv);
 
-  const response = await fetch("https://codex-vae6.onrender.com", {
+  const response = await fetch("http://codex-vae6.onrender.com", {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
